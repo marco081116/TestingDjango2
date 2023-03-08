bf4b061be09bc6079be2ff1e8864e832098618eb
Marco Nguyen <bao1230802@gmail.com>
2023-03-08 10:24:39 +0700

    user registration

--- Testing2/base/views.py
+++ Testing2/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse # xài cho update restriction
 from django.db.models import Q  # Q look up allows us to and or while searching
 from django.contrib import messages # ném message cho user
+from django.contrib.auth.decorators import login_required # decorater ti
 from django.contrib.auth.models import User # bộ library của django để đăng nhập user
 from django.contrib.auth import authenticate, login, logout # bộ library để login
+from django.contrib.auth.forms import UserCreationForm # bộ library để tạo rgister form
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -16,6 +19,10 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('Username')
         password = request.POST.get('Password')
@@ -32,7 +39,7 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password does not correct !')
 
-    context = {}
+    context = {'page': page}
 
     return render(request, 'base/login_register.html', context)
 
@@ -40,6 +47,23 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerPage(request):
+    # page = 'register' # dont need any more due to the form
+    form = UserCreationForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False) # take this form and freezing it in time
+            user.username = user.username.lower() # we can be able to 'claen' the account
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registerating !!!')
+    return render(request, 'base/login_register.html', context)
+ 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else '' # chứa cái tên topics để ném vào
 
@@ -65,6 +89,7 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+@login_required(login_url = 'login') # restriction
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -76,9 +101,13 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url = 'login')
 def updateRoom(request, pk):
     room = Room.objects.get(id = pk)
     form = RoomForm(instance= room)
+
+    # if request.user != room.host:
+    #     return HttpResponse('You are not allowed here !!!')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance = room)
@@ -89,8 +118,13 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url = 'login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id = pk)
+
+    # if request.user != room.host:
+    #     return HttpResponse('You are not allowed here !!!')
+
     if request.method == 'POST':
         room.delete()
         return redirect('home') # chuyen ve home (nhu o create room)
