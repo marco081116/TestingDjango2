d4bd0583485e5e9850b98e7ea504ca32febbfbb8
Marco Nguyen <bao1230802@gmail.com>
2023-03-18 16:51:30 +0700

    done theme installation

--- Testing2/base/views.py
+++ Testing2/base/views.py
@@ -118,32 +118,53 @@ def userProfile(request, pk):
 @login_required(login_url = 'login') # restriction
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all() # đây là để cho user có options (dynamic) khi tạo phòng
     if request.method == 'POST':
         # print(request.POST) # testing back-end
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit = False)
-            room.host = request.user
-            room.save()
-            return redirect('home') # chuyen ve home
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name) 
+        # nếu pass cái topic_name vào name, thì nó sẽ ra cái có sẵn
+        # nếu ko có sẵn cái để pass (new value), sẽ tạo thêm cái đó vào list cho ngời dùng
+        
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit = False)
+        #     room.host = request.user
+        #     room.save()
+        
+        return redirect('home') # chuyen ve home
+         
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url = 'login')
 def updateRoom(request, pk):
     room = Room.objects.get(id = pk)
     form = RoomForm(instance= room)
+    topics = Topic.objects.all() # đây là để cho user có options (dynamic) khi tạo phòng
 
     # if request.user != room.host:
     #     return HttpResponse('You are not allowed here !!!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance = room)
-        if form.is_valid():
-            form.save()
-            return redirect('home') # chuyen ve home (nhu o create room)
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name) 
+        # form = RoomForm(request.POST, instance = room)
+        # if form.is_valid():
+        #     form.save()
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home') # chuyen ve home (nhu o create room)
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url = 'login')
